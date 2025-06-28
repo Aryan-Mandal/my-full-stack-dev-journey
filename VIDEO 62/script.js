985fde66612d208ae39def7ef27f5f909a0400b6
Aryan-Mandal <aryanmandal2303@gmail.com>
2025-06-29 01:44:49 +0530

    main body of netfix UI

--- VIDEO 62/script.js
+++ VIDEO 62/script.js
@@ -1,4 +1,4 @@
-function nameGenerator(x, y, z) {
+function nameGenerator1(x, y, z) {
   let num = Math.floor(Math.random() * 3);
   if (num == 0) {
     return x;
@@ -8,5 +8,33 @@ function nameGenerator(x, y, z) {
     return z;
   }
 }
-console.alert("This is a business name generator.");
-console.alert("A random business name will be generated once you press");
+function nameGenerator2(x, y, z) {
+  let num = Math.floor(Math.random() * 3);
+  if (num == 0) {
+    return x;
+  } else if (num == 1) {
+    return y;
+  } else {
+    return z;
+  }
+}
+function nameGenerator3(x, y, z) {
+  let num = Math.floor(Math.random() * 3);
+  if (num == 0) {
+    return x;
+  } else if (num == 1) {
+    return y;
+  } else {
+    return z;
+  }
+}
+alert("This is a business name generator.");
+alert("A random business name will be generated once you press ok");
+alert(
+  "Business name is: " +
+    nameGenerator1("Crazy", "Amazing", "Fire") +
+    " " +
+    nameGenerator2("Engine", "Foods", "Garments") +
+    " " +
+    nameGenerator3("Bros", "Limited", "Hub")
+);
